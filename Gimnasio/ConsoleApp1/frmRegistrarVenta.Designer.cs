0853b17a90d1308f42175004eb5785be5efa8a6a
Fabian Orccon <fabian.orccon@pucp.pe>
2017-04-25 23:42:03 +0200

    vista: Cambiar diseno de frmRegistrarVenta

--- Gimnasio/ConsoleApp1/frmRegistrarVenta.Designer.cs
+++ Gimnasio/ConsoleApp1/frmRegistrarVenta.Designer.cs
@@ -37,14 +37,17 @@
             this.btnEliminar = new System.Windows.Forms.Button();
             this.btnImprimir = new System.Windows.Forms.Button();
             this.lblCantProd = new System.Windows.Forms.Label();
-            this.txtCantProd = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblIdProd
             // 
             this.lblIdProd.AutoSize = true;
-            this.lblIdProd.Location = new System.Drawing.Point(73, 36);
+            this.lblIdProd.Location = new System.Drawing.Point(49, 79);
             this.lblIdProd.Name = "lblIdProd";
             this.lblIdProd.Size = new System.Drawing.Size(67, 13);
             this.lblIdProd.TabIndex = 0;
@@ -54,7 +57,7 @@
             // lblNombProd
             // 
             this.lblNombProd.AutoSize = true;
-            this.lblNombProd.Location = new System.Drawing.Point(73, 70);
+            this.lblNombProd.Location = new System.Drawing.Point(49, 110);
             this.lblNombProd.Name = "lblNombProd";
             this.lblNombProd.Size = new System.Drawing.Size(108, 13);
             this.lblNombProd.TabIndex = 1;
@@ -62,29 +65,29 @@
             // 
             // txtIdProd
             // 
-            this.txtIdProd.Location = new System.Drawing.Point(229, 36);
+            this.txtIdProd.Location = new System.Drawing.Point(176, 76);
             this.txtIdProd.Name = "txtIdProd";
-            this.txtIdProd.Size = new System.Drawing.Size(100, 20);
+            this.txtIdProd.Size = new System.Drawing.Size(293, 20);
             this.txtIdProd.TabIndex = 2;
             // 
             // txtNombProd
             // 
-            this.txtNombProd.Location = new System.Drawing.Point(229, 67);
+            this.txtNombProd.Location = new System.Drawing.Point(176, 107);
             this.txtNombProd.Name = "txtNombProd";
-            this.txtNombProd.Size = new System.Drawing.Size(99, 20);
+            this.txtNombProd.Size = new System.Drawing.Size(129, 20);
             this.txtNombProd.TabIndex = 3;
             // 
             // dgvProductos
             // 
             this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProductos.Location = new System.Drawing.Point(42, 165);
+            this.dgvProductos.Location = new System.Drawing.Point(52, 153);
             this.dgvProductos.Name = "dgvProductos";
-            this.dgvProductos.Size = new System.Drawing.Size(510, 181);
+            this.dgvProductos.Size = new System.Drawing.Size(510, 219);
             this.dgvProductos.TabIndex = 4;
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(465, 136);
+            this.btnAgregar.Location = new System.Drawing.Point(52, 392);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(75, 23);
             this.btnAgregar.TabIndex = 5;
@@ -94,7 +97,7 @@
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(42, 352);
+            this.btnEliminar.Location = new System.Drawing.Point(133, 392);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(75, 23);
             this.btnEliminar.TabIndex = 6;
@@ -103,7 +106,7 @@
             // 
             // btnImprimir
             // 
-            this.btnImprimir.Location = new System.Drawing.Point(465, 352);
+            this.btnImprimir.Location = new System.Drawing.Point(487, 392);
             this.btnImprimir.Name = "btnImprimir";
             this.btnImprimir.Size = new System.Drawing.Size(75, 23);
             this.btnImprimir.TabIndex = 7;
@@ -113,25 +116,44 @@
             // lblCantProd
             // 
             this.lblCantProd.AutoSize = true;
-            this.lblCantProd.Location = new System.Drawing.Point(73, 105);
+            this.lblCantProd.Location = new System.Drawing.Point(351, 110);
             this.lblCantProd.Name = "lblCantProd";
             this.lblCantProd.Size = new System.Drawing.Size(52, 13);
             this.lblCantProd.TabIndex = 8;
             this.lblCantProd.Text = "Cantidad:";
             // 
-            // txtCantProd
+            // numericUpDown1
             // 
-            this.txtCantProd.Location = new System.Drawing.Point(229, 98);
-            this.txtCantProd.Name = "txtCantProd";
-            this.txtCantProd.Size = new System.Drawing.Size(100, 20);
-            this.txtCantProd.TabIndex = 9;
+            this.numericUpDown1.Location = new System.Drawing.Point(409, 108);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(153, 20);
+            this.numericUpDown1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(487, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(52, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(510, 21);
+            this.comboBox1.TabIndex = 12;
             // 
             // frmRegistrarVenta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(623, 400);
-            this.Controls.Add(this.txtCantProd);
+            this.ClientSize = new System.Drawing.Size(623, 441);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.lblCantProd);
             this.Controls.Add(this.btnImprimir);
             this.Controls.Add(this.btnEliminar);
@@ -142,9 +164,10 @@
             this.Controls.Add(this.lblNombProd);
             this.Controls.Add(this.lblIdProd);
             this.Name = "frmRegistrarVenta";
-            this.Text = "Form1";
+            this.Text = "Registrar Venta";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,6 +184,8 @@
         private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.Button btnImprimir;
         private System.Windows.Forms.Label lblCantProd;
-        private System.Windows.Forms.TextBox txtCantProd;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
