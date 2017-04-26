4b2631973ac29e713d20bddb96d38f8d3afd3dd9
jfiestas <fiestas.jm@gmail.com>
2017-04-25 19:32:06 -0500

    Proyecto: CrearEmpleado

---
+++ Gimnasio/ConsoleApp1/frmCrearEmpleado.designer.cs
@@ -0,0 +1,379 @@
+﻿namespace ConsoleApp1
+{
+    partial class frmCrearEmpleado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFechaNacimiento = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtDistrito = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtApMat = new System.Windows.Forms.TextBox();
+            this.txtApPat = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblFechaNacimiento = new System.Windows.Forms.Label();
+            this.lblDistrito = new System.Windows.Forms.Label();
+            this.lblApMat = new System.Windows.Forms.Label();
+            this.lblApPat = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDni = new System.Windows.Forms.Label();
+            this.gbDatosPers1 = new System.Windows.Forms.GroupBox();
+            this.lblCargo = new System.Windows.Forms.Label();
+            this.lblTurno = new System.Windows.Forms.Label();
+            this.cmbTurno = new System.Windows.Forms.ComboBox();
+            this.cmbHorario = new System.Windows.Forms.ComboBox();
+            this.lblHorario = new System.Windows.Forms.Label();
+            this.lblCuenta = new System.Windows.Forms.Label();
+            this.txtCuenta = new System.Windows.Forms.TextBox();
+            this.cmbCargo = new System.Windows.Forms.ComboBox();
+            this.gbOficio = new System.Windows.Forms.GroupBox();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.gbDatosPers1.SuspendLayout();
+            this.gbOficio.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtFechaNacimiento
+            // 
+            this.txtFechaNacimiento.Location = new System.Drawing.Point(488, 309);
+            this.txtFechaNacimiento.Name = "txtFechaNacimiento";
+            this.txtFechaNacimiento.Size = new System.Drawing.Size(117, 20);
+            this.txtFechaNacimiento.TabIndex = 17;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(398, 278);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(206, 20);
+            this.txtCorreo.TabIndex = 16;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(486, 246);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(119, 20);
+            this.txtTelefono.TabIndex = 15;
+            // 
+            // txtDistrito
+            // 
+            this.txtDistrito.Location = new System.Drawing.Point(398, 212);
+            this.txtDistrito.Name = "txtDistrito";
+            this.txtDistrito.Size = new System.Drawing.Size(207, 20);
+            this.txtDistrito.TabIndex = 14;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(346, 178);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(260, 20);
+            this.txtDireccion.TabIndex = 13;
+            // 
+            // txtApMat
+            // 
+            this.txtApMat.Location = new System.Drawing.Point(398, 131);
+            this.txtApMat.Name = "txtApMat";
+            this.txtApMat.Size = new System.Drawing.Size(208, 20);
+            this.txtApMat.TabIndex = 12;
+            // 
+            // txtApPat
+            // 
+            this.txtApPat.Location = new System.Drawing.Point(398, 100);
+            this.txtApPat.Name = "txtApPat";
+            this.txtApPat.Size = new System.Drawing.Size(208, 20);
+            this.txtApPat.TabIndex = 11;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(398, 68);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(208, 20);
+            this.txtNombre.TabIndex = 10;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(486, 37);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(120, 20);
+            this.txtDni.TabIndex = 9;
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(35, 178);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(55, 13);
+            this.lblDireccion.TabIndex = 7;
+            this.lblDireccion.Text = "Direccion:";
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Location = new System.Drawing.Point(35, 278);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(41, 13);
+            this.lblCorreo.TabIndex = 5;
+            this.lblCorreo.Text = "Correo:";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(35, 246);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(52, 13);
+            this.lblTelefono.TabIndex = 8;
+            this.lblTelefono.Text = "Telefono:";
+            // 
+            // lblFechaNacimiento
+            // 
+            this.lblFechaNacimiento.AutoSize = true;
+            this.lblFechaNacimiento.Location = new System.Drawing.Point(35, 309);
+            this.lblFechaNacimiento.Name = "lblFechaNacimiento";
+            this.lblFechaNacimiento.Size = new System.Drawing.Size(111, 13);
+            this.lblFechaNacimiento.TabIndex = 2;
+            this.lblFechaNacimiento.Text = "Fecha de Nacimiento:";
+            // 
+            // lblDistrito
+            // 
+            this.lblDistrito.AutoSize = true;
+            this.lblDistrito.Location = new System.Drawing.Point(35, 212);
+            this.lblDistrito.Name = "lblDistrito";
+            this.lblDistrito.Size = new System.Drawing.Size(42, 13);
+            this.lblDistrito.TabIndex = 6;
+            this.lblDistrito.Text = "Distrito:";
+            // 
+            // lblApMat
+            // 
+            this.lblApMat.AutoSize = true;
+            this.lblApMat.Location = new System.Drawing.Point(68, 176);
+            this.lblApMat.Name = "lblApMat";
+            this.lblApMat.Size = new System.Drawing.Size(89, 13);
+            this.lblApMat.TabIndex = 14;
+            this.lblApMat.Text = "Apellido Materno:";
+            // 
+            // lblApPat
+            // 
+            this.lblApPat.AutoSize = true;
+            this.lblApPat.Location = new System.Drawing.Point(68, 145);
+            this.lblApPat.Name = "lblApPat";
+            this.lblApPat.Size = new System.Drawing.Size(87, 13);
+            this.lblApPat.TabIndex = 13;
+            this.lblApPat.Text = "Apellido Paterno:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(68, 113);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(52, 13);
+            this.lblNombre.TabIndex = 12;
+            this.lblNombre.Text = "Nombres:";
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(68, 82);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(29, 13);
+            this.lblDni.TabIndex = 11;
+            this.lblDni.Text = "DNI:";
+            // 
+            // gbDatosPers1
+            // 
+            this.gbDatosPers1.Controls.Add(this.lblDireccion);
+            this.gbDatosPers1.Controls.Add(this.txtFechaNacimiento);
+            this.gbDatosPers1.Controls.Add(this.txtCorreo);
+            this.gbDatosPers1.Controls.Add(this.txtTelefono);
+            this.gbDatosPers1.Controls.Add(this.txtDistrito);
+            this.gbDatosPers1.Controls.Add(this.txtDireccion);
+            this.gbDatosPers1.Controls.Add(this.txtApMat);
+            this.gbDatosPers1.Controls.Add(this.txtApPat);
+            this.gbDatosPers1.Controls.Add(this.txtNombre);
+            this.gbDatosPers1.Controls.Add(this.txtDni);
+            this.gbDatosPers1.Controls.Add(this.lblCorreo);
+            this.gbDatosPers1.Controls.Add(this.lblTelefono);
+            this.gbDatosPers1.Controls.Add(this.lblFechaNacimiento);
+            this.gbDatosPers1.Controls.Add(this.lblDistrito);
+            this.gbDatosPers1.Location = new System.Drawing.Point(33, 44);
+            this.gbDatosPers1.Name = "gbDatosPers1";
+            this.gbDatosPers1.Size = new System.Drawing.Size(622, 346);
+            this.gbDatosPers1.TabIndex = 15;
+            this.gbDatosPers1.TabStop = false;
+            this.gbDatosPers1.Text = "Datos Personales";
+            // 
+            // lblCargo
+            // 
+            this.lblCargo.AutoSize = true;
+            this.lblCargo.Location = new System.Drawing.Point(34, 29);
+            this.lblCargo.Name = "lblCargo";
+            this.lblCargo.Size = new System.Drawing.Size(38, 13);
+            this.lblCargo.TabIndex = 16;
+            this.lblCargo.Text = "Cargo:";
+            // 
+            // lblTurno
+            // 
+            this.lblTurno.AutoSize = true;
+            this.lblTurno.Location = new System.Drawing.Point(34, 61);
+            this.lblTurno.Name = "lblTurno";
+            this.lblTurno.Size = new System.Drawing.Size(38, 13);
+            this.lblTurno.TabIndex = 18;
+            this.lblTurno.Text = "Turno:";
+            // 
+            // cmbTurno
+            // 
+            this.cmbTurno.FormattingEnabled = true;
+            this.cmbTurno.Location = new System.Drawing.Point(480, 53);
+            this.cmbTurno.Name = "cmbTurno";
+            this.cmbTurno.Size = new System.Drawing.Size(121, 21);
+            this.cmbTurno.TabIndex = 19;
+            // 
+            // cmbHorario
+            // 
+            this.cmbHorario.FormattingEnabled = true;
+            this.cmbHorario.Location = new System.Drawing.Point(482, 89);
+            this.cmbHorario.Name = "cmbHorario";
+            this.cmbHorario.Size = new System.Drawing.Size(121, 21);
+            this.cmbHorario.TabIndex = 21;
+            // 
+            // lblHorario
+            // 
+            this.lblHorario.AutoSize = true;
+            this.lblHorario.Location = new System.Drawing.Point(35, 92);
+            this.lblHorario.Name = "lblHorario";
+            this.lblHorario.Size = new System.Drawing.Size(94, 13);
+            this.lblHorario.TabIndex = 20;
+            this.lblHorario.Text = "Horario de trabajo:";
+            // 
+            // lblCuenta
+            // 
+            this.lblCuenta.AutoSize = true;
+            this.lblCuenta.Location = new System.Drawing.Point(35, 123);
+            this.lblCuenta.Name = "lblCuenta";
+            this.lblCuenta.Size = new System.Drawing.Size(44, 13);
+            this.lblCuenta.TabIndex = 18;
+            this.lblCuenta.Text = "Cuenta:";
+            // 
+            // txtCuenta
+            // 
+            this.txtCuenta.Location = new System.Drawing.Point(346, 123);
+            this.txtCuenta.Name = "txtCuenta";
+            this.txtCuenta.Size = new System.Drawing.Size(260, 20);
+            this.txtCuenta.TabIndex = 19;
+            // 
+            // cmbCargo
+            // 
+            this.cmbCargo.FormattingEnabled = true;
+            this.cmbCargo.Location = new System.Drawing.Point(480, 20);
+            this.cmbCargo.Name = "cmbCargo";
+            this.cmbCargo.Size = new System.Drawing.Size(121, 21);
+            this.cmbCargo.TabIndex = 17;
+            // 
+            // gbOficio
+            // 
+            this.gbOficio.Controls.Add(this.lblCuenta);
+            this.gbOficio.Controls.Add(this.txtCuenta);
+            this.gbOficio.Controls.Add(this.lblCargo);
+            this.gbOficio.Controls.Add(this.cmbHorario);
+            this.gbOficio.Controls.Add(this.cmbCargo);
+            this.gbOficio.Controls.Add(this.lblHorario);
+            this.gbOficio.Controls.Add(this.lblTurno);
+            this.gbOficio.Controls.Add(this.cmbTurno);
+            this.gbOficio.Location = new System.Drawing.Point(33, 414);
+            this.gbOficio.Name = "gbOficio";
+            this.gbOficio.Size = new System.Drawing.Size(622, 153);
+            this.gbOficio.TabIndex = 22;
+            this.gbOficio.TabStop = false;
+            this.gbOficio.Text = "Datos de oficio";
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Location = new System.Drawing.Point(579, 602);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(75, 23);
+            this.btnFinalizar.TabIndex = 23;
+            this.btnFinalizar.Text = "Finalizar";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            // 
+            // frmCrearEmpleado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 683);
+            this.Controls.Add(this.btnFinalizar);
+            this.Controls.Add(this.lblApMat);
+            this.Controls.Add(this.lblApPat);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblDni);
+            this.Controls.Add(this.gbDatosPers1);
+            this.Controls.Add(this.gbOficio);
+            this.Name = "frmCrearEmpleado";
+            this.Text = "frmCrearEntrenador";
+            this.gbDatosPers1.ResumeLayout(false);
+            this.gbDatosPers1.PerformLayout();
+            this.gbOficio.ResumeLayout(false);
+            this.gbOficio.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFechaNacimiento;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtDistrito;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtApMat;
+        private System.Windows.Forms.TextBox txtApPat;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblFechaNacimiento;
+        private System.Windows.Forms.Label lblDistrito;
+        private System.Windows.Forms.Label lblApMat;
+        private System.Windows.Forms.Label lblApPat;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.GroupBox gbDatosPers1;
+        private System.Windows.Forms.Label lblCargo;
+        private System.Windows.Forms.Label lblTurno;
+        private System.Windows.Forms.ComboBox cmbTurno;
+        private System.Windows.Forms.ComboBox cmbHorario;
+        private System.Windows.Forms.Label lblHorario;
+        private System.Windows.Forms.Label lblCuenta;
+        private System.Windows.Forms.TextBox txtCuenta;
+        private System.Windows.Forms.ComboBox cmbCargo;
+        private System.Windows.Forms.GroupBox gbOficio;
+        private System.Windows.Forms.Button btnFinalizar;
+    }
+}
