7681592175b0ca5a4a47168933aa5d1f9d04c519
alulab14 <alulab14@V20715>
2017-04-26 09:33:11 -0500

    Proyecto: aagregar atributo de monto a producto

--- Gimnasio/ConsoleApp1/CLASES/ProductoXCliente.cs
+++ Gimnasio/ConsoleApp1/CLASES/ProductoXCliente.cs
@@ -12,18 +12,21 @@ namespace ConsoleApp1
         private int _idProducto;
         private DateTime _fechaCompra;
         private int _cantAdq;
+        private double _monto;
 
-        public ProductoXCliente(int dni, int idProducto, DateTime fechaCompra, int cantAdq)
+        public ProductoXCliente(int dni, int idProducto, DateTime fechaCompra, int cantAdq, double monto)
         {
             _dni = dni;
             _idProducto = idProducto;
             _fechaCompra = fechaCompra;
             _cantAdq = cantAdq;
+            _monto = monto;
         }
 
         public int Dni { get => _dni; set => _dni = value; }
         public int IdProducto { get => _idProducto; set => _idProducto = value; }
         public DateTime FechaCompra { get => _fechaCompra; set => _fechaCompra = value; }
         public int CantAdq { get => _cantAdq; set => _cantAdq = value; }
+        public double Monto { get => _monto; set => _monto = value; }
     }
 }
